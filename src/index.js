3d5fcac216585ba4b56df0e4c7bdb13554720a8a
sarahjx <swang311@my.bcit.ca>
2025-09-15 19:11:54 -0700

    fixing linking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // optional if you have global styles
+import App from './App.js';
+import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
