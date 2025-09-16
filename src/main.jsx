c6c21d1c9556eca782713cbd531f1a9a86f17f48
sarahjx <swang311@my.bcit.ca>
2025-09-15 17:50:51 -0700

    changed favicon, started book ui jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { useState, useEffect } from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+useEffect(() => {
+    async function fetchBooks() {
+      try {
+        const results = await Promise.all(
+        isbnList.map((isbn) =>
+          fetch(`https://api.itbook.store/1.0/books/${isbn}`).then((response) =>
+            response.json()
+          )
+        )
+        );
+      } catch (error) {
+        console.error("No books found", error);
+  }
+}
+
+fetchBooks();
+}, []);
+
+// function BookDisplay(props) {
+//   const linkLabel = "Learn More";
+//   (
+//     <div>
+//       <img className="book-image">{props.image}</img>
+//       <h2 className="book-title">{props.title}</h2>
+//       <h3 className="book-author">{props.author}</h3>
+//       <a className="book-link">{linkLabel}</a>
+//     </div>
+// );
+// }
